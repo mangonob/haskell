3a4cbece31ed3eab19a05510bee5d8a2ef17c13c
mangonob <mangonob@qq.com>
2021-04-01 13:07:58 +0800

    feat: Random

--- app/Main.hs
+++ app/Main.hs
@@ -1,44 +1,21 @@
 module Main where
 
-import Data.Char
-import System.Directory
-import System.Environment
-import System.IO
+import System.Random
 
 main = do
-  (command : args) <- getArgs
-  case command of
-    "add" -> add args
-    "view" -> view args
-    "remove" -> remove args
-    _ -> return ()
+  gen <- newStdGen
+  putStrLn $ take 20 $ randomRs ('a', 'z') gen
 
-add :: [String] -> IO ()
-add (name : text : _) = appendFile name text
-add _ = return ()
+threeCoins :: StdGen -> (Bool, Bool, Bool)
+threeCoins gen =
+  let (first, newGen) = random gen
+      (second, newGen') = random newGen
+      (third, _) = random newGen'
+   in (first, second, third)
 
-view :: [String] -> IO ()
-view (name : _) = do
-  contents <- readFile name
-  putStr $ unlines $ zipWith ordLine [0 ..] $ lines contents
-  where
-    ordLine i l = show i ++ " - " ++ l
-view _ = return ()
-
-remove :: [String] -> IO ()
-remove (name : no : _) = do
-  contents <- readFile name
-  (tName, tHandle) <- openTempFile "." "temp"
-  let num = read no :: Integer
-      newContents = unlines $ filterWithIndex (\i _ -> i /= num) $lines contents
-  hPutStr tHandle newContents
-  hClose tHandle
-  removeFile name
-  renameFile tName name
-  where
-    onlyLine (_, l) = l
-remove _ = return ()
-
-filterWithIndex :: (Integer -> a -> Bool) -> [a] -> [a]
-filterWithIndex _ [] = []
-filterWithIndex f xs = map (\(_, l) -> l) $ filter (\(i, l) -> f i l) $ zip [0 ..] xs
+finiteRandoms :: (Integral a1, Random a2, RandomGen b, Num a1) => a1 -> b -> ([a2], b)
+finiteRandoms 0 gen = ([], gen)
+finiteRandoms n gen =
+  let (r, gen') = random gen
+      (xs, gen'') = finiteRandoms (n - 1) gen'
+   in (r : xs, gen'')
