4cc97689e60855478169dd49fdc3a7fd9144428c
mangonob <mangonob@qq.com>
2021-04-07 00:33:47 +0800

    feat: Writer Monad

--- app/Main.hs
+++ app/Main.hs
@@ -1,31 +1,15 @@
 module Main where
 
-import Control.Applicative
+import Control.Monad.Writer
+import Data.DList
 
 main :: IO ()
 main = putStrLn "42"
 
-sqA :: Applicative f => [f a] -> f [a]
-sqA [] = pure []
-sqA (x : xs) = (:) <$> x <*> sqA xs
-
-type KnightPos = (Int, Int)
-
-moveKnight :: KnightPos -> [KnightPos]
-moveKnight (c, r) =
-  filter
-    onBoard
-    [ (c + 2, r + 1),
-      (c + 2, r - 1),
-      (c - 2, r + 1),
-      (c - 2, r - 1),
-      (c + 1, r + 2),
-      (c + 1, r - 2),
-      (c - 1, r + 2),
-      (c - 1, r - 2)
-    ]
-  where
-    onBoard (c, r) = (elem c [1 .. 8]) && (elem r [1 .. 8])
-
-in3 :: KnightPos -> [KnightPos]
-in3 start = return start >>= moveKnight >>= moveKnight >>= moveKnight
+gcd' :: Int -> Int -> Writer [String] Int
+gcd' a 0 = do
+  tell ["Finished with " ++ show a]
+  return a
+gcd' a b = do
+  tell [show a ++ " mod " ++ show b ++ " = " ++ show (mod a b)]
+  gcd' b (mod a b)
