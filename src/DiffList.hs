fac2f62e9c486c4256c3c6895405590f7b06c09b
mangonob <mangonob@qq.com>
2021-04-08 01:39:04 +0800

    WIP: DiffList

---
+++ src/DiffList.hs
@@ -0,0 +1,18 @@
+module DiffList () where
+
+import Data.Monoid
+
+newtype DiffList a = DiffList {getDiffList :: [a] -> [a]}
+
+toDiffList :: [a] -> DiffList a
+toDiffList xs = DiffList (xs ++)
+
+fromDiffList :: DiffList a -> [a]
+fromDiffList (DiffList f) = f []
+
+instance Semigroup (DiffList a) where
+  f <> g = mappend f g
+
+instance Monoid (DiffList a) where
+  mempty = DiffList (\xs -> [] ++ xs)
+  (DiffList f) `mappend` (DiffList g) = DiffList (\xs -> f (g xs))
