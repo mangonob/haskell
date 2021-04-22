e400cccf4d56bbcf6c1d8fc4b41f9fcb9e2d3eb0
mangonob <mangonob@qq.com>
2021-04-23 00:40:24 +0800

    refactor: Add package time

--- app/Main.hs
+++ app/Main.hs
@@ -3,10 +3,3 @@ module Main where
 main :: IO ()
 main = do
   putStrLn $ show 42
-
-fibo :: Int -> Integer
-fibo = (map memo [1 ..] !!)
-  where
-    memo 1 = 1
-    memo 2 = 1
-    memo x = fibo (x - 1) + fibo (x - 2)
