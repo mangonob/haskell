723f7bd42ebfe9be905d782fbe27b9fdbf44acd6
mangonob <mangonob@qq.com>
2021-04-25 23:34:44 +0800

    feat: RSA

---
+++ src/Crypt/Prime.hs
@@ -0,0 +1,33 @@
+module Crypt.Prime (isPrime, modPow) where
+
+depart :: (Num a, Integral b) => b -> (a, b)
+depart n
+  | n <= 0 = error "Depart a nopositive number"
+  | mod n 2 == 0 = let (s, t) = depart $ div n 2 in (s + 1, t)
+  | otherwise = (0, n)
+
+isPrime :: Integer -> Bool
+isPrime x
+  | x < 30 = elem x primer
+  | otherwise = and $ millerRobin <$> primer <*> pure x
+
+millerRobin :: Integral a => a -> a -> Bool
+millerRobin p x =
+  let (s, t) = depart (x - 1)
+      testor = take (s + 1) $ iterate (modPow x 2) $ modPow x t p
+   in let det = and $ map (\n -> n == 1 || n == x - 1) $ filter (\n -> modPow x 2 n == 1) $ init testor
+       in det && (mod (last testor) x == 1)
+
+mod' :: Integral a => a -> a -> a
+mod' a b = mod b a
+
+modPow :: Integral a => a -> a -> a -> a
+modPow n 0 x = 1
+modPow n 1 x = mod x n
+modPow n p x
+  | x > n = modPow n p (mod' n x)
+  | mod p 2 == 0 = mod' n $ modPow n (div p 2) (x * x)
+  | otherwise = mod' n $ x * modPow n (div p 2) (x * x)
+
+primer :: [Integer]
+primer = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
---
+++ src/Crypt/RSA.hs
@@ -0,0 +1,49 @@
+module Crypt.RSA (genKey, encrypt, decrypt) where
+
+import Crypt.Prime
+import System.Random
+
+data Key = Private Integer Integer | Public Integer Integer deriving (Show)
+
+type KeyPair = (Key, Key)
+
+bitBounds :: (Integral a) => a -> (Integer, Integer)
+bitBounds x = (2 ^ x, 2 ^ (x + 1))
+
+defaultBounds :: (Integer, Integer)
+defaultBounds = bitBounds 512
+
+genKey :: IO KeyPair
+genKey = do
+  gen <- newStdGen
+  let (p, genQ) = genPrime defaultBounds gen
+  let (q, genE) = genPrime defaultBounds genQ
+  let n = p * q
+  let l = lcm (p - 1) (q - 1)
+  let (e, genD) = genR (\x -> gcd x l == 1) (2, l - 1) genE
+  let (_, d, _) = exEuclid e l
+  return (Public n e, Private n (mod (d + l) l))
+
+-- Gen prime number in range
+genPrime :: (Integer, Integer) -> StdGen -> (Integer, StdGen)
+genPrime = genR isPrime
+
+-- Gen number in range (filter by predicate)
+genR :: (Integer -> Bool) -> (Integer, Integer) -> StdGen -> (Integer, StdGen)
+genR f bs gen =
+  let (p, nextGen) = randomR bs gen
+   in if f p
+        then (p, nextGen)
+        else genR f bs nextGen
+
+encrypt :: Key -> Integer -> Integer
+encrypt (Public n e) x = modPow n e x
+
+decrypt :: Key -> Integer -> Integer
+decrypt (Private n d) x = modPow n d x
+
+exEuclid :: Integral c => c -> c -> (c, c, c)
+exEuclid a 0 = (a, 1, 0)
+exEuclid a b =
+  let (d', x', y') = exEuclid b (mod a b)
+   in (d', y', x' - (div a b) * y')
