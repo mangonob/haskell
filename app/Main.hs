26eae6be8915bc0b8facc5df1c00a291ad447c48
mangonob <mangonob@qq.com>
2021-04-26 23:51:08 +0800

    feat: Respond palindrome

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,7 @@
 module Main where
 
 main :: IO ()
-main = interact $ unlines . filter ((< 10) . length) . lines
+main = interact respondPalindromes
+
+respondPalindromes :: String -> String
+respondPalindromes = unlines . map (\x -> if x == reverse x then "palindrome" else "not a palindrome") . lines
