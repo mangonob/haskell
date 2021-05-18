c52d3061c6421b93a99c041e46a779b5671cfe9c
mangonob <mangonob@qq.com>
2021-05-18 10:50:56 +0800

    feat: Sum files

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,20 @@
 {-# LANGUAGE ViewPatterns #-}
 
-import Algo.Data.RBTree
-import Algo.Pretty
-import Utils
+import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString.Lazy.Char8 as C
+import Data.Char
+import System.Environment
 
 main :: IO ()
-main = interact id
+main = do
+  (fileName : _) <- getArgs
+  bs <- B.readFile fileName
+  putStrLn $ show $ sumBs bs
+
+sumBs :: B.ByteString -> Int
+sumBs (C.readInt -> Just (a, bs)) = a + sumBs (trim bs)
+sumBs (C.readInt -> Nothing) = 0
+
+trim :: B.ByteString -> B.ByteString
+trim (C.uncons -> Just ((isSpace -> True), ts)) = ts
+trim bs = bs
