8cc27cce9327393b1ad578525b297a81f615959e
mangonob <mangonob@qq.com>
2021-07-19 00:04:51 +0800

    feat: some Writer

--- app/Main.hs
+++ app/Main.hs
@@ -2,3 +2,11 @@ module Main where
 
 main :: IO ()
 main = interact id
+
+isBigGang :: Int -> (Bool, String)
+isBigGang x = (x >= 9, "Compare gang size to 9.")
+
+applyLog :: (a, String) -> (a -> (b, String)) -> (b, String)
+applyLog (x, log) f = let (v, newLog) = f x in (v, log ++ newLog)
+
+test = ("some text", "create some text.") `applyLog` (\x -> (length x, " get length of " ++ x))
