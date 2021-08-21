8ff60cbd570a39503ecab8abd85c9966ad4a4802
mangonob <mangonob@qq.com>
2021-08-21 21:31:39 +0800

    chore: Fix Hint

--- src/Algo/NumberTheory.hs
+++ src/Algo/NumberTheory.hs
@@ -4,7 +4,7 @@ exEuclid :: Integral c => c -> c -> (c, c, c)
 exEuclid a 0 = (a, 1, 0)
 exEuclid a b =
   let (d, x, y) = exEuclid b (mod a b)
-   in (d, y, x - (div a b) * y)
+   in (d, y, x - div a b * y)
 
 exGcd :: Integer -> Integer -> (Integer, Integer, Integer)
 exGcd = exEuclid
