321d9de6d495276a324f6bccc30b61e365a7b810
mangonob <mangonob@qq.com>
2021-10-25 00:47:00 +0800

    chore: Generatic STable

--- src/Compiler/Tiger/Semantic/STable.hs
+++ src/Compiler/Tiger/Semantic/STable.hs
@@ -6,29 +6,29 @@ import Compiler.Tiger.Semantic.Types
 import Compiler.Tiger.Symbol (Sym)
 import qualified Data.Map as Map
 import Data.Maybe (Maybe (Just, Nothing))
-import Prelude (Show, error)
+import Prelude (Show, error, ($))
 
-type Table = Map.Map Sym Type
+type Table a = Map.Map Sym a
 
-data STable = STable {prev :: Maybe STable, table :: Table} deriving (Show)
+data STable a = STable {prev :: Maybe (STable a), table :: Table a} deriving (Show)
 
-empty :: STable
+empty :: STable a
 empty = STable {prev = Nothing, table = Map.empty}
 
-singleton :: Sym -> Type -> STable
+singleton :: Sym -> a -> STable a
 singleton s t = STable {prev = Nothing, table = Map.singleton s t}
 
-insert :: Sym -> Type -> STable -> STable
+insert :: Sym -> a -> STable a -> STable a
 insert s t STable {prev = a, table = b} = STable {prev = a, table = Map.insert s t b}
 
-enter :: STable -> STable
+enter :: STable a -> STable a
 enter st = STable {prev = Just st, table = Map.empty}
 
-exit :: STable -> STable
+exit :: STable a -> STable a
 exit STable {prev = Nothing, table = _} = error "can not exit"
 exit STable {prev = Just st, table = b} = st
 
-lookup :: Sym -> STable -> Maybe Type
+lookup :: Sym -> STable a -> Maybe a
 lookup s STable {prev = a, table = b} = case Map.lookup s b of
   Just t -> Just t
   Nothing -> case a of
